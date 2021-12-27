1784f2e51fc20ab4729fc3c583016df244bdac11
zhaoyunxing <zhaoyunxing@apache.org>
2021-12-27 18:59:19 +0800

    add:钉钉事件处理

--- go.mod
+++ go.mod
@@ -3,8 +3,13 @@ module github.com/zhaoyunxing92/dingtalk-event
 go 1.17
 
 require (
+	github.com/gin-gonic/gin v1.7.7
+	github.com/zhaoyunxing92/dingtalk/v2 v2.0.0-00010101000000-000000000000
+	go.uber.org/zap v1.19.1
+)
+
+require (
 	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/gin-gonic/gin v1.7.7 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.9.0 // indirect
@@ -16,13 +21,13 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/ugorji/go/codec v1.2.6 // indirect
-	github.com/zhaoyunxing92/dingtalk/v2 v2.0.4 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.7.0 // indirect
-	go.uber.org/zap v1.19.1 // indirect
 	golang.org/x/crypto v0.0.0-20211215153901-e495a2d5b3d3 // indirect
 	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
+
+replace github.com/zhaoyunxing92/dingtalk/v2 => /Users/docker/code/github/go/dingtalk
--- main.go
+++ main.go
@@ -4,45 +4,45 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/zhaoyunxing92/dingtalk/v2"
+	"github.com/zhaoyunxing92/dingtalk/v2/crypto"
 	"go.uber.org/zap"
 )
 
 var client = dingtalk.NewClient(1244553273, "dingkjy4w80esdwgjuyo",
 	"bDKa_nfJg3zYRsFrj-wTohTuoJCtxTEHaGmybYF9vgaVAZJOz-mICsLGStB288nW", dingtalk.WithLevel(zap.DebugLevel))
 
-type Encrypt struct {
-	Text string `json:"encrypt"`
-}
-
 func main() {
 	rout := gin.Default()
-	rout.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
 
 	rout.POST("/dingtalk/event", func(ctx *gin.Context) {
 		fmt.Println(ctx)
 		sign := ctx.Query("msg_signature")
 		timestamp := ctx.Query("timestamp")
 		nonce := ctx.Query("nonce")
-		fmt.Println(sign)
-		fmt.Println(timestamp)
-		fmt.Println(nonce)
 
-		//buf := make([]byte, 4096)
-		//n, _ := ctx.Request.Body.Read(buf)
-		//fmt.Println(string(buf[0:n]))
+		en := crypto.DingTalkEncrypt{}
+		if err := ctx.BindJSON(&en); err != nil {
+			return
+		}
 
-		en := Encrypt{}
-		ctx.BindJSON(&en)
-		fmt.Println(en)
-		/*post_gwid := c.PostForm("name")
-		fmt.Println(post_gwid)*/
-		//msg_signature=4af305f33739df19e07c705fbb0c6272072fbe77&timestamp=1640447283988&nonce=Cnlz0NPV
+		fmt.Println(en.String())
 
+		dingCrypto := client.GetDingTalkCrypto("mUBLh4IahPjvQKOP", "ge4elNG3RwMTwV6MNMgpdvtMMeMpg3CuHlifOB9OeA6")
+
+		if msg, err := dingCrypto.Decrypt(en.Encrypt, sign, timestamp, nonce); err != nil {
+			fmt.Println(err)
+			return
+		} else {
+			fmt.Println(msg)
+		}
+
+		encrypt, err := dingCrypto.Encrypt("success")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		ctx.JSON(200, encrypt)
 	})
 
-	rout.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	rout.Run()
 }
