9848d479c35d92a8e6c7c5a1cc0593f1365ca1bf
zhaoyunxing <zhaoyunxing@apache.org>
2021-12-29 13:26:00 +0800

    add:log

--- main.go
+++ main.go
@@ -3,19 +3,38 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/zhaoyunxing92/dingtalk-event/logger"
 	"github.com/zhaoyunxing92/dingtalk/v2"
 	"github.com/zhaoyunxing92/dingtalk/v2/crypto"
 	"go.uber.org/zap"
 )
 
-var client = dingtalk.NewClient(1244553273, "dingkjy4w80esdwgjuyo",
-	"bDKa_nfJg3zYRsFrj-wTohTuoJCtxTEHaGmybYF9vgaVAZJOz-mICsLGStB288nW", dingtalk.WithLevel(zap.DebugLevel))
+var client = dingtalk.NewClient(1423051829, "dinggkluifooodnghss9",
+	"1wptnXj8VFHQKayDdcCkgbD-iiqoc_UnEPevIGQUszLrHDNrcRkTB52Z8sqXbym6",
+	dingtalk.WithLevel(zap.DebugLevel))
 
 func main() {
-	rout := gin.Default()
+	router := gin.New()
+	log := logger.InitLogger(zap.DebugLevel)
 
-	rout.POST("/dingtalk/event", func(ctx *gin.Context) {
-		fmt.Println(ctx)
+	router.Use(logger.GinLogger(log), logger.GinRecovery(log, true))
+
+	//router.Use(gin.LoggerWithFormatter(func(params gin.LogFormatterParams) string {
+	//	return fmt.Sprintf("%s - %s | %d | %s | %s | %s \n",
+	//		params.TimeStamp.Format("2006-01-02 03:04:05"),
+	//		params.ClientIP,
+	//		params.StatusCode,
+	//		params.Path,
+	//		params.Method,
+	//		params.Request.Proto,
+	//	)
+	//}))
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"msg": "pong",
+		})
+	})
+	router.POST("/dingtalk/event", func(ctx *gin.Context) {
 		sign := ctx.Query("msg_signature")
 		timestamp := ctx.Query("timestamp")
 		nonce := ctx.Query("nonce")
@@ -25,15 +44,13 @@ func main() {
 			return
 		}
 
-		fmt.Println(en.String())
-
 		dingCrypto := client.GetDingTalkCrypto("mUBLh4IahPjvQKOP", "ge4elNG3RwMTwV6MNMgpdvtMMeMpg3CuHlifOB9OeA6")
 
 		if msg, err := dingCrypto.Decrypt(en.Encrypt, sign, timestamp, nonce); err != nil {
-			fmt.Println(err)
+			log.Error("ding event err:", zap.Error(err))
 			return
 		} else {
-			fmt.Println(msg)
+			log.Info("ding event:", zap.String("res", msg))
 		}
 
 		encrypt, err := dingCrypto.Encrypt("success")
@@ -44,5 +61,5 @@ func main() {
 		ctx.JSON(200, encrypt)
 	})
 
-	rout.Run()
+	router.Run()
 }
